8630f73211c1412155b5af629778593e7783fd97
AlexandrDaultukaev <shyra31102000@mail.ru>
2020-07-20 12:12:04 +0700

    Add operator =

--- MyString/main.cpp
+++ MyString/main.cpp
@@ -1,6 +1,48 @@
 #include <iostream>
 
+class myString {
+	char* str;
+	int size = 0;
+public:
+
+	myString()
+	{
+		str = nullptr;
+	}
+
+	myString& operator = (const char* c)
+	{
+		while (c[size] != '\0')
+		{
+			size++;
+		}
+		str = new char[size+1];
+		for (int i = 0; i < size; i++)
+		{
+			str[i] = c[i];
+		}
+		str[size] = 0;
+		std::cout << this << std::endl;
+		return *this;
+	}
+
+	void ShowStr()
+	{
+		std::cout << str << std::endl;
+	}
+
+	~myString()
+	{
+		delete[] str;
+	}
+};
+
 int main()
 {
+
+	myString st;
+	st = "Hello World";
+	st.ShowStr();
+
 	return 0;
 }
