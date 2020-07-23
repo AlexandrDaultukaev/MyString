92e4b762966e496c6f6b751e9a59fb86f08082a5
AlexandrDaultukaev <shyra31102000@mail.ru>
2020-07-23 15:01:01 +0700

    Add some constructors

--- MyString/main.cpp
+++ MyString/main.cpp
@@ -2,47 +2,129 @@
 
 class myString {
 	char* str;
-	int size = 0;
+	int size;
 public:
 
 	myString()
 	{
 		str = nullptr;
+		size = 0;
 	}
 
 	myString& operator = (const char* c)
 	{
+		if (this->str != nullptr)
+		{
+			delete[] str;
+		}
 		while (c[size] != '\0')
 		{
 			size++;
 		}
+		//strlen(c);
 		str = new char[size+1];
 		for (int i = 0; i < size; i++)
 		{
 			str[i] = c[i];
 		}
-		str[size] = 0;
-		std::cout << this << std::endl;
+		str[size] = '\0';
+		//std::cout << this << std::endl;
 		return *this;
 	}
 
+
+	myString& operator = (const myString& c)
+	{
+		if (this->str != nullptr)
+		{
+			delete[] this->str;
+		}
+		this->size = strlen(c.str);
+		this->str = new char[this->size+1];
+		for (int i = 0; i < this->size; i++)
+		{
+			str[i] = c.str[i];
+		}
+		str[size] = '\0';
+		//std::cout << this << std::endl;
+		return *this;
+	}
+
+	myString operator + (const myString& c)
+	{
+		int llenstr = strlen(this->str);
+		int llenc = strlen(c.str);
+		myString sum;
+		sum.size = llenstr + llenc;
+		sum.str = new char[sum.size + 1];
+		int i = 0;
+		for (; i < strlen(this->str); i++)
+		{
+			sum.str[i] = this->str[i];
+		}
+		for (int j = 0; j < strlen(c.str); j++, i++)
+		{
+			sum.str[i] = c.str[j];
+		}
+		sum.str[sum.size] = '\0';
+		return sum;
+	}
+
+	myString(const char other[])
+	{
+		this->size = strlen(other);
+		this->str = new char[this->size + 1];
+
+		for (int i = 0; i < this->size; i++)
+		{
+			this->str[i] = other[i];
+		}
+
+		this->str[size] = '\0';
+	}
+
+	myString(const myString& other)
+	{
+		this->size = strlen(other.str);
+		this->str = new char[this->size + 1];
+
+		for (int i = 0; i < this->size; i++)
+		{
+			this->str[i] = other.str[i];
+		}
+
+		this->str[size] = '\0';
+	}
+
+	myString(char* c)
+	{
+		this->size = strlen(c);
+		this->str = new char[size + 1];
+		for (int i = 0; i < size; i++)
+		{
+			this->str[i] = c[i];
+		}
+		this->str[this->size] = '\0';
+	}
+
+
 	void ShowStr()
 	{
-		std::cout << str << std::endl;
+		std::cout << this->str << std::endl;
 	}
 
 	~myString()
 	{
-		delete[] str;
+		delete[] this->str;
 	}
 };
 
 int main()
 {
 
-	myString st;
-	st = "Hello World";
-	st.ShowStr();
+	//myString st;
+	//st = "Hello World";
+	//st.ShowStr();
 
 	return 0;
 }
